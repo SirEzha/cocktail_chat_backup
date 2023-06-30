a0c81b1ec7e1feff78e6e5f6b4fccdb4c17146fd
SirEzha <baginskiy.nhs@gmail.com>
2023-06-30 20:10:30 +0200

    test server

--- js/server.js
+++ js/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 443;
+const PORT = 80;
 const LanguageModel = require("./languageModel");
 const http = require('http');
 
@@ -14,37 +14,38 @@ const io = new Server(server, {
         allowedHeaders: ['Content-Type'],
     }
 });
-
-app.use(express.static(path.join(__dirname, '../chatbot/build')));
-
+//
+// app.use(express.static(path.join(__dirname, '../chatbot/build')));
+//
 app.get("/", (request, result) => {
-    result.sendFile(path.join(__dirname, '../chatbot/build/index.html'));
+    result.send('aboba')
+    // result.sendFile(path.join(__dirname, '../chatbot/build/index.html'));
 })
 server.listen(PORT, function () {
     console.log(`server started at port ${PORT}`);
 });
-
-io.on("connection", (socket) => {
-    console.log(`connect ${socket.id}`);
-
-    socket.on("disconnect", (reason) => {
-        console.log(`disconnect ${socket.id} due to ${reason}`);
-    });
-
-    const model = new LanguageModel();
-
-    socket.on("question", (data) => {
-        console.log("received question: "+data)
-        let answer;
-
-        try {
-            answer = model.getAnswer(data);
-        }
-        catch (e) {
-            answer = "An error occurred: " + e;
-        }
-        socket.emit("answer", answer);
-    });
-
-});
-
+//
+// io.on("connection", (socket) => {
+//     console.log(`connect ${socket.id}`);
+//
+//     socket.on("disconnect", (reason) => {
+//         console.log(`disconnect ${socket.id} due to ${reason}`);
+//     });
+//
+//     const model = new LanguageModel();
+//
+//     socket.on("question", (data) => {
+//         console.log("received question: "+data)
+//         let answer;
+//
+//         try {
+//             answer = model.getAnswer(data);
+//         }
+//         catch (e) {
+//             answer = "An error occurred: " + e;
+//         }
+//         socket.emit("answer", answer);
+//     });
+//
+// });
+//
