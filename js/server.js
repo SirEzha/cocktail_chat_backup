7e53f7471b3421b78eceecad44fd5dcddabcab27
SirEzha <baginskiy.nhs@gmail.com>
2023-06-30 20:22:01 +0200

    test5

--- js/server.js
+++ js/server.js
@@ -14,38 +14,37 @@ const io = new Server(server, {
         allowedHeaders: ['Content-Type'],
     }
 });
-//
-// app.use(express.static(path.join(__dirname, '../chatbot/build')));
-//
+
+app.use(express.static(path.join(__dirname, '../chatbot/build')));
+
 app.get("/", (request, result) => {
-    result.send('aboba')
-    // result.sendFile(path.join(__dirname, '../chatbot/build/index.html'));
+    result.sendFile(path.join(__dirname, '../chatbot/build/index.html'));
 })
 server.listen(PORT, function () {
     console.log(`server started at port ${PORT}`);
 });
-//
-// io.on("connection", (socket) => {
-//     console.log(`connect ${socket.id}`);
-//
-//     socket.on("disconnect", (reason) => {
-//         console.log(`disconnect ${socket.id} due to ${reason}`);
-//     });
-//
-//     const model = new LanguageModel();
-//
-//     socket.on("question", (data) => {
-//         console.log("received question: "+data)
-//         let answer;
-//
-//         try {
-//             answer = model.getAnswer(data);
-//         }
-//         catch (e) {
-//             answer = "An error occurred: " + e;
-//         }
-//         socket.emit("answer", answer);
-//     });
-//
-// });
-//
+
+io.on("connection", (socket) => {
+    console.log(`connect ${socket.id}`);
+
+    socket.on("disconnect", (reason) => {
+        console.log(`disconnect ${socket.id} due to ${reason}`);
+    });
+
+    const model = new LanguageModel();
+
+    socket.on("question", (data) => {
+        console.log("received question: "+data)
+        let answer;
+
+        try {
+            answer = model.getAnswer(data);
+        }
+        catch (e) {
+            answer = "An error occurred: " + e;
+        }
+        socket.emit("answer", answer);
+    });
+
+});
+
